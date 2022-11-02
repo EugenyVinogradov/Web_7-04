31eaf6a0efca97338e5003a4b86ce8874b370fbf
EugenyVinogradov <vinogradov_e_v@rambler.ru>
2022-11-02 22:37:55 +0300

    Task 3. Added tests for Netology.ru

--- gh.test.js
+++ gh.test.js
@@ -1,3 +1,5 @@
+const jestConfig = require("./jest.config");
+
 let page;
 
 
@@ -60,5 +62,38 @@ describe("Github loginPage tests", () => {
     });
     const actual = await page.$eval(btnSelector, link => link.getAttribute('data-signin-label'));
     expect(actual).toContain("Sign in")
+  }, 6000);
+});
+
+describe("Netology DevelopmentPage tests", () => {
+    
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://netology.ru/development");
+  }, 10000);
+
+  test("The h1 header content'", async () => {
+    await page.waitForSelector('h1.src-reallyShared-components-core-Heading--heading--PkavA.src-Landings-pages-Direction-components-Presentation--title--r7_mp');
+    const title2 = await page.title();
+    expect(title2).toEqual('Курсы программирования онлайн – обучение с нуля | Нетология');
+  }, 6000);
+
+  test("Open MiddleQA page", async () => {
+    const link = await page.$eval('[data-program-id="29630"]', link => link.getAttribute('href') );
+    await page.goto("https://netology.ru" + link);
+    const actual = await page.title();
+    expect(actual).toEqual("Тестировщик – обучение QA-инженеров на курсе в Нетологии");
+  }, 6000);
+
+  test("Switching to the course entry form MiddleQA", async () => {
+    const link = await page.$eval('[data-program-id="29630"]', link => link.getAttribute('href') );
+    await page.goto("https://netology.ru" + link);
+    await page.click("[name='button']");
+    const btnSelector = "[name='buttons.orderButton']";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toEqual("Записаться на курс");
   }, 6000);
 });
